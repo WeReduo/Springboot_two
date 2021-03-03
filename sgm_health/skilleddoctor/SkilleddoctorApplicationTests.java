3e84cc9acd39ff36f31a9060e9a00c5d5704c616
wenernduo <63387480+WeReduo@users.noreply.github.com>
2021-03-03 09:00:43 +0800

    这是一个看过视频学习做的项目
    这是一个健康医院药药瓶管理系统

---
+++ sgm_health/skilleddoctor/SkilleddoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgm_health.skilleddoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilleddoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
